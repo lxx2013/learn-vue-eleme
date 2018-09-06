66906967798a862e45f41e11f0a7e3a1df4025fd
lxx.imac <lxx2013@mail.ustc.edu.cn>
2018-09-06 18:05:40 +0800

    stylus-vscode配置调教的差不多了
    插件 Sync
    GitHub Token: 6a6eb0e3f079fd7a786b5f30c8e0dafcc279f608
    GitHub Gist: df0dcbe36dc20462d25aa0239356b87e
    GitHub Gist Type: Secret
    
    stylus插件(不要用 language-stylus)

---
+++ components/cartcontrol.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="cartcontrol">
+        <div class="cart-decrease" v-show="food.count>0"></div>
+        <div class="cart-count" v-show="food.count>0"></div>
+        <div class="cart-add"></div>
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    food: {
+      type: Object
+    }
+  },
+  created() {}
+};
+</script>
+<style lang="stylus" scoped>
+.test {
+  color #000
+  font-size 24px
+
+  .test2 {
+  }
+
+  .test3 {
+    background-color #fff
+    vertical-align top
+    line-height 24px
+  }
+}
+</style>
+
+
