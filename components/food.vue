cf87e1315be378d23f4daa91045c4666aa617019
lxx.imac <lxx2013@mail.ustc.edu.cn>
2018-09-09 22:29:32 +0800

    food.vue => 80% | [ rating √ ] / [ rating-js X ]

--- components/food.vue
+++ components/food.vue
@@ -28,6 +28,22 @@
           <div class="rating">
             <h1 class="title">商品评价</h1>
             <ratingselect :originSelectType="selectType"  :desc="desc" :ratings="food.ratings"></ratingselect>
+            <div class="rating-wrapper">
+              <ul v-show="food.ratings && food.ratings.length" style="list-style:none">
+                <li v-for="(rating,index) in food.ratings" :key="index" class="rating-item" >
+                  <div class="user">
+                    <span class="name">{{rating.username}}</span>
+                    <img class='avatar' width="12px" height="12px" :src="rating.avatar" alt="rating.avatar">
+                  </div>
+                  <div class="time">{{new Date(rating.rateTime).toLocaleString()}}</div>
+                  <p class="text">
+                    <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>
+                    {{rating.text}}
+                  </p>
+                </li>
+              </ul>
+              <div class="no-rating" v-show="!food.ratings||!food.ratings.length"></div>
+            </div>
           </div>
         </div>
     </div>
@@ -74,6 +90,8 @@ export default {
 };
 </script>
 <style lang='stylus' scoped>
+@import '~assets/mixin'
+
 .food-detail {
   position fixed
   left 0
@@ -169,11 +187,61 @@ export default {
 
   .rating {
     padding-top 18px
-    .title{
+
+    .title {
       line-height 14px
       margin-left 18px
       font-size 14px
       color rgb(7, 17, 27)
+    }
+
+    .rating-wrapper {
+      padding 0 18px
+
+      .rating-item {
+        position relative
+        padding 16px 0
+        border-1px(rgba(7, 17, 27, 0.1))
+
+        .user {
+          position absolute
+          right 0
+          top 16px
+          line-height 12px
+
+          .name {
+            display inline-block
+            vertical-align top
+            font-size 10px
+            color rgb(147, 153, 159)
+          }
+          .avatar {
+            border-radius 50%
+          }
+        }
+        .time{
+          line-height 12px
+          font-size 10px
+          color rgb(147, 153, 159)
+          margin-bottom 6px
+        }
+        .text{
+          line-height 16px
+          font-size 12px
+          color rgb(7,17,27)
+          .icon-thumb_up,.icon-thumb_down{
+            margin-right 4px
+            line-height 16px
+            font-size 12px
+          }
+          .icon-thumb_up{
+              color rgb(0,160,220)
+          }
+          .icon-thumb_down{
+              color rgb(140,153,159)
+          }
+        }
+      }
     }
   }
 }
