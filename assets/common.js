742933865def10c162a2ab3f376160f5d31a8988
lxx.imac <lxx2013@mail.ustc.edu.cn>
2018-09-11 16:49:49 +0800

    给日期不足1位的补0

--- assets/common.js
+++ assets/common.js
@@ -3,14 +3,14 @@ export function formatDate(date,fmt){
         fmt = fmt.replace(RegExp.$1,(date.getFullYear()+'').substr(4-RegExp.$1.length));
     }
     let o = {
-        'M+':date.getMonth()+1,
-        'd+':date.getDate(),
-        'h+':date.getHours(),
-        'm+':date.getSeconds()
+        'M+':(date.getMonth()+1)+'',
+        'd+':date.getDate()+'',
+        'h+':date.getHours()+'',
+        'm+':date.getSeconds()+''
     }
     for(let k in o){
         if(new RegExp(`(${k})`).test(fmt)){
-            fmt = fmt.replace(RegExp.$1,(o[k]+''));
+            fmt = fmt.replace(RegExp.$1,(o[k].length!=1 ? o[k] : '0'+o[k]));
         }
     }
     return fmt;
